92c7446a0abc2ecb93c8712841633934ece278b9
Jose-Tejero <j_jtejero@hotmail.com>
2022-11-15 18:17:07 -0600

    inicialización de la bd de un chat tipo WhatsApp

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const db = require('./utils/database');
+const handleError = require('./middlewares/error.middleware');
+const initModels = require('./models/initModels');
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+initModels();
+
+db.authenticate()
+  .then(() => console.log('Autenticación exitosa'))
+  .catch((error) => console.log(error));
+
+db.sync({ alter: true })
+  .then(() => console.log('Base de datos sincronizada'))
+  .catch((error) => console.log(error));
+
+app.get('/', () => {
+  console.log('Todo bien');
+});
+
+app.use(handleError);
+
+module.exports = app;
