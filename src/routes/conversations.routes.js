6357c857947c345bcf7bee2e56dc4dd9b8d31218
Jose-Tejero <j_jtejero@hotmail.com>
2022-11-18 20:56:07 -0600

    Agregando el controlador a las rutas de conversaciones

--- src/routes/conversations.routes.js
+++ src/routes/conversations.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
+const { getUserConversations } = require('../controllers');
 const authenticate = require('../middlewares/auth.middlewares');
 
 const router = Router();
 
-router.get('/conversations/:id', authenticate, () => { });
+router.get('/conversations/:id', authenticate, getUserConversations);
 
 module.exports = router;
